c8a1110a56705fac4c32e77d1818bf95ee34b750
Damiann Kurus <Damiannkurus@gmail.com>
2021-06-07 13:27:17 -0400

    game completed and works in console, will add GUI and update code in future

--- idk.js
+++ idk.js
@@ -1,71 +1,102 @@
+//holds the score, will be used to compare to determine winner
+let computerScore;
+let playerScore; 
 
+//randomizes a whole number from 1-3, 1=rock, 2=paper, 3=scissors
 function computerPlay (min = 1, max = 4){
    let oneTwoThree = Math.floor(Math.random() * (max - min) + min);
    let result; 
    if (oneTwoThree === 1){
-       result = 'Rock';
+       result = 'rock';
    }
    else if (oneTwoThree === 2) {
-       result = 'Paper';
+       result = 'paper';
    }
    else {
-       result = 'Scissors';
+       result = 'scissors';
    }
    return result;
 }
 
+//plays a single round of RPS, takes the computers selection and players selection and compares them to determine result and update score.
 function playRound (playerSelection, computerSelection) {
-
     let playerInput = playerSelection.toLowerCase();
 
     if (playerInput === 'rock'){
         if (computerSelection === 'rock'){
-            return ('Rock against Rock, draw!');
+            return alert('Rock against Rock, draw!');
         }
         else if (computerSelection === 'paper') {
-            return ('Rock against Paper, you lose!');
+            ++computerScore;
+            return alert('Rock against Paper, you lose!');
         }
         else if (computerSelection === 'scissors') {
-            return ('Rock against scissors, you win!');
+            ++playerScore;
+            return alert('Rock against scissors, you win!');
         }
         else {
-            return ('woops, looks like something went wrong')
+            return alert('woops, looks like something went wrong')
         }
     }
     else if (playerInput === 'paper'){
         if (computerSelection === 'paper'){
-            return ('Paper against Rock, you win!');
+            ++playerScore;
+            return alert('Paper against Rock, you win!');
         }
         else if (computerSelection === 'paper') {
-            return ('Paper against Paper, draw!');
+            return alert('Paper against Paper, draw!');
         }
         else if (computerSelection === 'scissors') {
-            return ('Paper against scissors, you lose!');
+            ++computerScore;
+            return alert('Paper against scissors, you lose!');
         }
         else {
-            return ('woops, looks like something went wrong')
+            return alert('woops, looks like something went wrong')
         }
     }
     else if (playerInput === 'scissors') {
         if (computerSelection === 'scissors'){
-            return ('Scissors against Rock, you lose!');
+            ++computerScore;
+            return alert('Scissors against Rock, you lose!');
         }
         else if (computerSelection === 'paper') {
-            return ('Scissors against Paper, you win!');
+            ++playerScore;
+            return alert('Scissors against Paper, you win!');
         }
         else if (computerSelection === 'scissors') {
-            return ('Scissors against scissors, draw!');
+            return alert('Scissors against scissors, draw!');
         }
         else {
-            return ('woops, looks like something went wrong')
+            return alert('woops, looks like something went wrong')
         }
     }
     else {
-        return ('woops, looks like something went wrong')
+        return alert('woops, looks like something went wrong')
     }
  
 }
 
+//asking for user input and running the function above to play game
 function game () {
-    
+    let playerSelection = prompt("type in one of the following three choices. rock, paper, or scissors.");
+    playRound(playerSelection, computerPlay());
+
+}
+
+// best of 5 
+game();
+game();
+game();
+game();
+game();  
+
+//final score check
+if (playerScore > computerScore) {
+    console.log("you win!")
+}      
+else if (playerScore < computerScore){
+    console.log('you lose!');
+}
+else {
+    console.log('tie game!')
 }
